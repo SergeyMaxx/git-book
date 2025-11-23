dae8e2a0473bb70f7a161753045736592524240e
Sergey Maximov <95900933+SergeyMaxx@users.noreply.github.com>
2025-11-23 10:42:13 +0300

    BACK-0001 тестирование индексирования.

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
       </div>
 
       <h1>Vite + React</h1>
+      <h1>Vite + React</h1>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
